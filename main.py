1b3132a5bea490a4de1480c9b7d2b88eed16a44d
Pr0t0n <105520163+Pr0t0ns@users.noreply.github.com>
2022-12-04 17:54:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from discord.ext import commands
 
 cog_files = ['cogs.api_commands', 'cogs.moderation', 'cogs.stat_commands', 'cogs.economy', 'cogs.fun_commands', 'cogs.ads']
 
-TOKEN = "MTA0OTA4NTgzMTk3MDgyMDI0Ng.GAXGpY.92ssOSCUUmCAfnrgrogGqHEMW-smiHPKr8AaMU"
+TOKEN = ""
 client = commands.Bot(command_prefix='')
